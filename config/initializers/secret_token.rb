d1fc3a9681d22fbccdcba7a0edfc660262f2534d
Heidi <heidi.harman@gmail.com>
2014-04-05 21:03:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refinerylabs::Application.config.secret_key_base = '9e7ac9e63688f9b3d95c3f84a0538cebfc6dfd0679f134565a523a746853d4eed99ccf9d9c24ebf54e6ffdc6b226e059a4a88532a086ec4eefff86ce45173078'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Refinerylabs::Application.config.session_store :cookie_store, key: '_refinerylabs_session'
