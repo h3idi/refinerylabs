7695cc86ac07e6cada63e6f45eb7f8fec1f74dde
Heidi <heidi.harman@gmail.com>
2014-04-16 11:40:47 -0700

    added clients page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Refinerylabs::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" 
   get "home" => "pages#home" 
+  get "clients" => "pages#clients"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
